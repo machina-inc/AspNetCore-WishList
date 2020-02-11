1079a4c8ea8bba084ebe195727f0cf1749a9126c
Steven Cuneo <scuneo@aosmith.com>
2020-02-11 16:36:34 -0600

    Home Controller

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -1,1 +1,13 @@
-﻿using Microsoft.
+﻿using Microsoft.AspNetCore.Mvc;
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View("Index");
+    }
+
+    public IActionResult Error()
+    {
+        return View("Error");
+    }
+}
