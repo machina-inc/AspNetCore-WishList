e029477bec3b6bd1c68541dab54434a87b03bb5f
Steven Cuneo <scuneo@aosmith.com>
2020-02-11 16:56:03 -0600

    Fixing

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-public class HomeController : Controller
+namespace WishList.Controllers
 {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View("Index");
-    }
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
 
-    public IActionResult Error()
-    {
-        return View("Error");
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
     }
 }
