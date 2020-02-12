062dad9bbc92a2540581c37fc88217e097655452
Steven Cuneo <scuneo@aosmith.com>
2020-02-12 09:10:24 -0600

    Item Model with EntityFramework Support

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
